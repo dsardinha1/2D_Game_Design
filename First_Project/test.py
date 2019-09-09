b01fa34e30a958915e405a04a89009769c1a1555
Davina <dsardinha@student.bridgew.edu>
2019-09-09 13:14:23 -0400

    yolo

--- First_Project/test.py
+++ First_Project/test.py
@@ -1,1 +1,8 @@
-#i, how are you :)
+"""simple python program for learning"""
+#initial file
+#add second comment
+import typing
+def main():
+    demo tuple = (1,2,3)
+    x,y,z = demo tuple
+    print(F" x: {x}, y : {y}, z: {z}")
