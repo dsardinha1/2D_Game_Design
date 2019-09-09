b8723a37fac673e6fc0a51dffc67257f00bb60a7
Davina <dsardinha@student.bridgew.edu>
2019-09-09 13:36:38 -0400

    hello world version

--- First_Project/test.py
+++
@@ -1,8 +0,0 @@
-"""simple python program for learning"""
-#initial file
-#add second comment
-import typing
-def main():
-    demo tuple = (1,2,3)
-    x,y,z = demo tuple
-    print(F" x: {x}, y : {y}, z: {z}")
