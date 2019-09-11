0e06b26c3ad113a8468b097b6a078640c3835bbf
Davina Sardinha <dsardinha@student.bridgew.edu>
2019-09-11 12:19:41 -0400

    hello world version

---
+++ First_Project/test.py
@@ -0,0 +1,11 @@
+"""simple python program for learning"""
+#initial file
+#add second comment
+import math
+import typing
+def main():
+    test = "hello world"
+    print(test)
+    quit()
+
+main()
